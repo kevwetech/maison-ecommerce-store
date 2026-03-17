74d1c736e30e57b561ad453001e730447e9f9954
Godson Oyibayado <godsonoyibayado@gmail.com>
2026-03-17 14:44:07 +0100

    Ready for live deployment

--- ecommerceproject/settings.py
+++ ecommerceproject/settings.py
@@ -130,7 +130,7 @@ EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True') == 'True'
 EMAIL_USE_SSL = os.environ.get('EMAIL_USE_SSL', 'False') == 'True'
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
-DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', EMAIL_HOST_USER or 'noreply@maison.com')
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', EMAIL_HOST_USER or 'godsonoyibayado@gmail.com')
 EMAIL_TIMEOUT = 10  # ADD THIS - prevents hanging
 
 ADMIN_EMAIL = os.environ.get('ADMIN_EMAIL')
