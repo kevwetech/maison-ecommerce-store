57d20f20827d97838f0ea6921ff8cb16410bab92
Godson Oyibayado <godsonoyibayado@gmail.com>
2026-03-17 19:16:58 +0100

    Force collectstatic on deploy

--- ecommerceproject/settings.py
+++ ecommerceproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'temporary-fallback-key-change-this')
 
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'  # FIX: Use env var
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'  # FIX: Use env var
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 
@@ -110,8 +110,10 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
