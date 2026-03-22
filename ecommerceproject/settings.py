9f655c9543e3ac65b4fe669a40772c7969f557df
Godson Oyibayado <godsonoyibayado@gmail.com>
2026-03-22 14:14:16 +0100

    Fix Cloudinary

--- ecommerceproject/settings.py
+++ ecommerceproject/settings.py
@@ -146,6 +146,10 @@ STORAGES = {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
