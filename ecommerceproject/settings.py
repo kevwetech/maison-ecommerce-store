c67ff4becccbedd96ab6fb735e0dcc346b989ab7
Godson Oyibayado <godsonoyibayado@gmail.com>
2026-03-22 14:05:28 +0100

    Fix Cloudinary

--- ecommerceproject/settings.py
+++ ecommerceproject/settings.py
@@ -84,7 +84,7 @@ CLOUDINARY_STORAGE = {
 
 print("CLOUDINARY_CLOUD_NAME:", os.environ.get('CLOUDINARY_CLOUD_NAME'))
 print("CLOUDINARY_API_KEY:", os.environ.get('CLOUDINARY_API_KEY'))
-print("DEFAULT_FILE_STORAGE:", DEFAULT_FILE_STORAGE)
+
 
 MESSAGE_TAGS = {
     messages.DEBUG: 'debug',
