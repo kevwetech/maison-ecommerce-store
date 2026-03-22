e8d2cb088203658de3837edb46142cf496b98b7e
Godson Oyibayado <godsonoyibayado@gmail.com>
2026-03-23 00:02:26 +0100

    Fix STATICFILES_DIRS path

--- ecommerceproject/settings.py
+++ ecommerceproject/settings.py
@@ -134,6 +134,10 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'products', 'static'),
+    os.path.join(BASE_DIR, 'static'),
+]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STORAGES = {
     "default": {
